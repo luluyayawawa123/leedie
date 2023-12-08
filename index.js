21902ea1ad505e8dfb3602df6368537dbf659694
luluyayawawa123 <129629792+luluyayawawa123@users.noreply.github.com>
2023-12-08 22:32:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 
 const server = http.createServer(function(req, res) {
-  proxy.web(req, res, { target: 'http://u2.bksy1105.com:5244' });
+  proxy.web(req, res, { target: 'http://u2.bksy1105.com:8880' });
 });
 
 server.listen(process.env.PORT || 3000, () => {
