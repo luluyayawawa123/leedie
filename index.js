937295a8d315ab8014c0037c22adc1e5e7c92203
luluyayawawa123 <129629792+luluyayawawa123@users.noreply.github.com>
2023-12-08 21:36:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ const server = http.createServer(function(req, res) {
   proxy.web(req, res, { target: 'http://u2.bksy1105.com:8880' });
 });
 
-console.log("Proxy server running on port 3000");
-server.listen(3000);
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Proxy server running");
+});
